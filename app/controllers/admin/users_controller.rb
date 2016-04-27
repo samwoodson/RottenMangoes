eb7486a3d5978e198b2ddf52ee678420c646dcb0
samwoodson <samuelwoodson@gmail.com>
2016-04-27 01:29:35 +0000

    added user edit/update functionality

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,5 +12,24 @@ class Admin::UsersController < ApplicationController
     redirect_to admin_users_path
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
+  end
+
+protected
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :admin)
+  end
 
 end
