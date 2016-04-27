107fb0ac26a84f680325467f1b1d0b1e810607e6
samwoodson <samuelwoodson@gmail.com>
2016-04-27 00:18:56 +0000

    added admin feature to delete user

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,16 @@
+class Admin::UsersController < ApplicationController
+
+  before_filter :restrict_admin_access
+
+  def index 
+    @users = User.all
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
+
+end
